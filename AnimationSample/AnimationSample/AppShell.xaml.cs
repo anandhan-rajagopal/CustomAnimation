bcaf3c7f02d7c9f88845bffb3f040076352ab17c
Anandhan <anandhan.rajagopal@syncfusion.com>
2023-08-01 21:26:42 +0530

    Added example sample for custom animation in .NET MAUI

---
+++ AnimationSample/AnimationSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimationSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AnimationSample/AnimationSample/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Animations;
+
+namespace AnimationSample;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		var scaleAnimation = new Microsoft.Maui.Controls.Animation(OnUpdateAnimation, 0, 1, Easing.BounceIn, OnAnimationEnded);
+		var animationManager = Handler.MauiContext.Services.GetRequiredService<IAnimationManager>();
+		scaleAnimation.Commit(animationManager);	
+	}
+
+    private void OnAnimationEnded()
+    {
+		label.FontSize = 14;
+    }
+
+    private void OnUpdateAnimation(double obj)
+    {
+        label.FontSize = label.FontSize + 0.5;
+    }
+}
+
